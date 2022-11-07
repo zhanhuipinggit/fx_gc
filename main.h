2eb7b54a1d998a73024a68135c4d3b58ded221f0
PHP-湛慧平 <zhanhuiping@taqu.cn>
2022-11-07 20:44:52 +0800

    虚拟机和简单GC

---
+++ main.h
@@ -0,0 +1,47 @@
+#define STACK_MAX 256
+#define INITIAL_GC_THRESHOLD 10000
+
+
+typedef enum {
+    OBJ_INT,
+    OBJ_PAIR
+} ObjectType;
+
+
+typedef struct sObject {
+    struct sObject* next;
+    unsigned char marked;
+    ObjectType type;
+
+    union {
+        /* OBJ_INT */
+        int value;
+
+        /* OBJ_PAIR */
+        struct {
+            struct sObject* head;
+            struct sObject* tail;
+        };
+    };
+} Object;
+typedef struct {
+    /* The total number of currently allocated objects. */
+    int numObjects;
+
+    /* The number of objects required to trigger a GC. */
+    int maxObjects;
+
+    Object* firstObject;
+    Object* stack[STACK_MAX];
+    int stackSize;
+} VM;
+void gc(VM* vm);
+VM* newVM();
+void push(VM* vm, Object* value);
+Object* pop(VM* vm);
+Object* newObject(VM* vm, ObjectType type);
+void mark(Object* object);
+void markAll(VM* vm);
+void pushInt(VM* vm, int intValue);
+Object* pushPair(VM* vm);
+void sweep(VM* vm);
